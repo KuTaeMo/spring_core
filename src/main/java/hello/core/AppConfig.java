2700016bf131897127c1f3e2e2e3c390ccd48220
KUTAEMO <kutaemo211@gmail.com>
2023-02-18 21:38:51 +0900

    1. 싱글톤 컨테이너 - 싱글톤 패턴 - 스프링에서 싱글톤 패턴을 지켜주는 방법(CGLIB)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,40 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    // MemoryMemberRepository 객체가 두번 생성되는 것 같은데 싱글톤이 깨질까 안깨질까? -> 안깨진다! 모두 동일하다
+
+    // 예상 호출 순서 @Configuration 제거 시 이렇게 출력됨
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // 실제 호출 순서 -> 스프링(@Configuration)이 싱글톤을 보장해준다!
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
+    // 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서
+    // AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것이다
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
